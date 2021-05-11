90aefb28fd3583f5d79bbd54d0f81f8742662981
Shamsiddin <shamsiddin2000.09@gmail.com>
2021-05-11 09:09:35 +0200

    settings

--- fakecsv/settings.py
+++ fakecsv/settings.py
@@ -138,7 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
@@ -146,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'media'),
 )
 
 # Celery configs
